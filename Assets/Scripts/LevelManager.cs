4ac46e1a8a4fe6689cbd6ab54ab611171aaa7cc7
Victor B <victormbeas@gmail.com>
2016-03-18 21:48:50 -0500

    First Commit
    Updating assets

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name) {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitRequest() {
+        Application.Quit();
+        Debug.Log("I Want to quit!");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 15.0f;
+    public float padding = 1f;
+
+    float xMin;
+    float xMax;
+
+    void Start() {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xMin = leftMost.x + padding;
+        xMax = rightMost.x - padding;
+    }
+	// Update is called once per frame
+	void Update () {
+        PlayerMovement();
+	}
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            //transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            //transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        // restrict the player to the gamespace.
+        float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
